fcab5fb2dc445a78cf30a57fd5afbf70c030e77c
Ana Carolina Santos <149174247+Carolrbs@users.noreply.github.com>
2025-06-08 16:41:02 -0300

    Add files via upload

---
+++ Lista2 exe7/Lista2 exe7/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_exe7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double lado1;
+            double lado2;
+            double lado3;
+
+            Console.WriteLine("Vamos verificar se três valores formam um triângulo e qual tipo ele é.");
+
+            Console.Write("Digite o primeiro lado: ");
+            lado1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo lado: ");
+            lado2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o terceiro lado: ");
+            lado3 = double.Parse(Console.ReadLine());
+
+            // Verifica existência de triângulo
+            // A soma de dois lados deve ser maior que o terceiro lado
+            if (lado1 < lado2 + lado3 && lado2 < lado1 + lado3 && lado3 < lado1 + lado2)
+            {
+                // Classifica o triângulo
+                // Triângulo Equilátero: todos os lados iguais
+                if (lado1 == lado2 && lado2 == lado3)
+                {
+                    Console.WriteLine("Os lados formam um triângulo EQUILÁTERO.");
+                }
+
+                // Triângulo Isósceles: dois lados iguais
+                else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+                {
+                    Console.WriteLine("Os lados formam um triângulo ISÓSCELES.");
+                }
+                
+                // Triângulo Escaleno: todos os lados diferentes
+                else
+                {
+                    Console.WriteLine("Os lados formam um triângulo ESCALENO.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Os valores informados NÃO formam um triângulo.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
