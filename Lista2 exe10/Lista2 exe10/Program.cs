848ce63c8d937ae90c438daf5a10ccd49c53870c
Ana Carolina Santos <149174247+Carolrbs@users.noreply.github.com>
2025-06-08 18:53:37 -0300

    Add files via upload

---
+++ Lista2 exe10/Lista2 exe10/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_exe10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+;
+            Console.WriteLine(" CALCULADORA DE MÉDIA SEMESTRAL DE ALUNO ");
+            Console.WriteLine("Vamos calcular a média das suas duas avaliações (P1 e P2).");
+            Console.WriteLine("Lembre-se: P2 tem peso dobrado na média!");
+
+            double notaP1;
+            double notaP2;
+            double mediaFinal; 
+
+            Console.Write("Digite a nota da P1 (Primeira Prova): ");
+            notaP1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a nota da P2 (Segunda Prova): ");
+            notaP2 = double.Parse(Console.ReadLine());
+
+            // Cálculo da Média
+            // MEDIA = (P1 + 2 * P2) / 3
+            mediaFinal = (notaP1 + (2 * notaP2)) / 3; 
+
+            Console.WriteLine($"Sua média final calculada é: {mediaFinal }");
+
+            //  Verificar a Situação do Aluno (Aprovado ou Reprovado)
+            if (mediaFinal >= 5.0)
+            {
+                // Se a condição (mediaFinal >= 5.0) for VERDADEIRA, o aluno está APROVADO.
+                Console.WriteLine(" SITUAÇÃO: ALUNO APROVADO! Parabéns! ");
+
+            }
+            else // Se a condição acima for FALSA (ou seja, mediaFinal é menor que 5.0)
+            {
+                // Se a condição for FALSA, o aluno está REPROVADO.
+                Console.WriteLine(" SITUAÇÃO: ALUNO REPROVADO. Estude mais. ");
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para sair do programa...");
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ Lista2 exe9/Lista2 exe9/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_exe9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso;
+            double altura;
+            double sexoNumero;
+
+
+
+            Console.WriteLine(" CALCULADORA DE ÍNDICE DE MASSA CORPORAL (IMC) ");
+
+            Console.Write("Por favor, digite o seu peso em quilogramas (kg) (ex: 65.5): ");
+            peso = double.Parse(Console.ReadLine());
+
+            Console.Write("Agora, digite a sua altura em metros (m) (ex: 1.75): ");
+            altura = double.Parse(Console.ReadLine());
+
+            Console.Write("Por fim, digite o seu sexo (1 para Feminino, 2 para Masculino): ");
+            sexoNumero = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            //Cálculo do IMC (Índice de Massa Corporal)
+            double alturaAoQuadrado = altura * altura;
+            double imc = peso / alturaAoQuadrado;
+
+            Console.WriteLine($"Seu IMC calculado é: ");
+
+            //  Categoria do IMC com base no Sexo Numérico 1 ouu 2
+            Console.WriteLine("Sua categoria de peso é:");
+
+            // se o sexo é Feminino (representado por 1)
+            if (sexoNumero == 1.0)
+            {
+                // ategorias específicas para mulheres
+                if (imc < 19.0) // Se o IMC for menor que 19
+                {
+                    Console.WriteLine("Abaixo do peso");
+                }
+                else if (imc <= 19.0 && imc < 24.0)
+                {
+                    Console.WriteLine("Peso ideal");
+                }
+                else if (imc >= 24.0)
+                {
+                    Console.WriteLine("Acima do peso");
+
+                }
+                // se o sexo é Masculino (representado por 2)
+                else if (sexoNumero == 2.0)
+                {
+                    if (imc < 20.0)
+                    {
+                        Console.WriteLine("Abaixo do peso");
+                    }
+                    else if (imc <= 20.0 && imc < 25.0)
+                    {
+                        Console.WriteLine("Peso ideal");
+                    }
+                    else if (imc >= 25.0)
+                    {
+                        Console.WriteLine("Acima do peso");
+                    }
+                    // Se o número digitado não for 1 nem 2
+                    else
+                    {
+                        Console.WriteLine("Opção de sexo não reconhecida. Por favor, digite 1 ou 2.");
+                    }
+
+                    Console.WriteLine();
+                    Console.WriteLine(" Pressione qualquer tecla para sair...");
+                    Console.ReadKey(); // Espera o usuário pressionar uma tecla antes de fechar o console
+                }
+            }
+        }
+    }
+}
