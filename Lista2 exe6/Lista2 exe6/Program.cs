4f468b846fb4be88807b84f49cd0f2eb9d75af4d
Ana Carolina Santos <149174247+Carolrbs@users.noreply.github.com>
2025-06-08 16:22:52 -0300

    Add files via upload

---
+++ Lista2 exe6/Lista2 exe6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_exe6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso;
+            double altura;
+            double r;
+
+            Console.WriteLine("Vamos verificar seu peso conforme sua altura.");
+            Console.Write("Digite seu peso (kg): ");
+            peso = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite sua altura (m): ");
+            altura = double.Parse(Console.ReadLine());
+
+            r = peso / (altura * altura);
+            if (r < 20)
+            {
+                Console.WriteLine("Você está com baixo peso.");
+            }
+            else if (r >= 20 && r < 25)
+            {
+                Console.WriteLine("Você está com peso ideal.");
+            }
+            else // r >= 25
+            {
+                Console.WriteLine("Você está acima do peso.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
